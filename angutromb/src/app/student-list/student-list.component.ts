4a63bca8eb8ad03a2b079611804fab1233bf7c24
CapitaineGloomy <n.chambon@outlook.fr>
2019-11-21 15:48:26 +0100

    Student-list-component et creation de l'interface student.ts dans app C'pour toi théo <3

--- angutromb/src/app/student-list/student-list.component.ts
+++ angutromb/src/app/student-list/student-list.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from "../student";
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+
 
 @Component({
   selector: 'app-student-list',
@@ -14,12 +10,15 @@ export interface Tile {
 })
 export class StudentListComponent implements OnInit {
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
+
+  students: Student[] = [];
+
+  newStudent: string;
+
+  private refreshTodos() {
+    const todos = localStorage.getItem('todos')
+    this.students = todos ? JSON.parse(todos) : []
+  }
 
   constructor() { }
 
---
+++ angutromb/src/app/student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+
+
+        nom: string
+        blabla: string
+        blablabla: string
+    
+    
+
+}
