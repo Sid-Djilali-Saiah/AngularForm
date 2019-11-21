04795f1903ab93118cd42e43892a4fbcd68cefa8
Sid <sid.djilali-saiah@epitech.eu>
2019-11-21 11:25:01 +0100

    form.component.ts updated : image upload and preview + app.component.ts updated : toolbar Added

--- angutromb/src/app/form/form.component.ts
+++ angutromb/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  myForm: FormGroup;
+  imageURL: string;
 
-  constructor() { }
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.fb.group({
+      avatar: [null],
+      firstName: [''],
+      lastName: [''],
+      dob: ['']
+    })
+  }
+
+  showPreview(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.myForm.patchValue({
+      avatar: file
+    });
+    this.myForm.get('avatar').updateValueAndValidity()
+
+    // File Preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+    }
+    reader.readAsDataURL(file)
+  }
+
+  // Submit Form
+  onSubmit() {
+    console.log(this.myForm.value)
   }
 
 }
